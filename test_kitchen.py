81b67cf3d493e1a6d8f2fe02bfd39fe85146035e
boatngin <thanapat.boat16@gmail.com>
2026-09-03 11:57:35 +0700

    A5: units

--- test_kitchen.py
+++ test_kitchen.py
@@ -7,7 +7,9 @@ from kitchen import Quantity
 #     flour.times(2)
 #     assert flour.amount == 400
 
-def test_equality():
-    assert Quantity(200) == Quantity(200)
-    assert Quantity(200) != Quantity(300)
+# def test_equality():
+#     assert Quantity(200) == Quantity(200)
+#     assert Quantity(200) != Quantity(300)
 
+def test_grams_are_not_ounces():
+    assert Quantity(1, "g") != Quantity(1, "oz")
