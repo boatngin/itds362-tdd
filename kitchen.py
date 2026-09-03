19af97bc2f58bbf49ac74219c6220a1759ae0c8c
boatngin <thanapat.boat16@gmail.com>
2026-09-03 11:49:10 +0700

    A2: triangulated multiplication

--- kitchen.py
+++ kitchen.py
@@ -4,4 +4,5 @@ class Quantity:
         self.amount = amount
  
     def times(self, multiplier):
-        self.amount = 600
+        self.amount = self.amount * multiplier
+
--- test_kitchen.py
+++ test_kitchen.py
@@ -2,7 +2,7 @@
 from kitchen import Quantity
  
  
-def test_multiplication():
+def test_multiplication_by_two():
     flour = Quantity(200)
-    flour.times(3)
-    assert flour.amount == 600
+    flour.times(2)
+    assert flour.amount == 400
