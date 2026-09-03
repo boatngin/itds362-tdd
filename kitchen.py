6945099ce40580c187211509ffae5bc8b0aedd56
boatngin <thanapat.boat16@gmail.com>
2026-09-03 11:52:19 +0700

    A3-A4: quantity is a value object; equality

--- kitchen.py
+++ kitchen.py
@@ -4,5 +4,12 @@ class Quantity:
         self.amount = amount
  
     def times(self, multiplier):
-        self.amount = self.amount * multiplier
+        return Quantity(self.amount * multiplier)
 
+
+
+    def __eq__(self, other):
+        return self.amount == other.amount
+ 
+    def __repr__(self):
+        return f"Quantity({self.amount})"
--- test_kitchen.py
+++ test_kitchen.py
@@ -2,7 +2,12 @@
 from kitchen import Quantity
  
  
-def test_multiplication_by_two():
-    flour = Quantity(200)
-    flour.times(2)
-    assert flour.amount == 400
+# def test_multiplication_by_two():
+#     flour = Quantity(200)
+#     flour.times(2)
+#     assert flour.amount == 400
+
+def test_equality():
+    assert Quantity(200) == Quantity(200)
+    assert Quantity(200) != Quantity(300)
+
